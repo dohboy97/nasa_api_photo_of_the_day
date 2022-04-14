c52ab0b04f8b059f140cc4f1e6328e2e355fb5f6
Connor <connor.doherty97@gmail.com>
2022-04-14 14:43:57 -0400

    added a calendar to select a custom date, and made it so that every submission will clear the original HTML elements

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 
+document.querySelector('.submit').addEventListener('click', getAPOD)
 
+function getAPOD(){
+let date = document.querySelector('input').value
 
-fetch ('https://api.nasa.gov/planetary/apod?date=2022-03-30&api_key=Blevdngru4uXheqWFtLTAe7qMdXMox5NJj8nf0of')
+document.querySelector('iframe').classList.add('invisible')
+
+fetch (`https://api.nasa.gov/planetary/apod?date=${date}&api_key=Blevdngru4uXheqWFtLTAe7qMdXMox5NJj8nf0of`)
     .then(res => res.json())
     .then(data => {
         console.log(data);
 
+        //clearing everything on initial run
+        document.querySelector('img').src = '';
+
+        document.querySelector('.date').innerHTML = '';
+
+        document.querySelector('p').innerHTML = '';
+
+        document.querySelector('.copyright').innerHTML = ''
+
         document.querySelector('h2').innerHTML = data.title;
+
+        
+
+        //if APOD is an img
+
 
         if(data.media_type === 'image'){
         document.querySelector('img').src = data.hdurl;
-
-        }else if (data.media_type === 'video'){
-            document.querySelector('iframe').classList.toggle('invisible')
-            document.querySelector('iframe').src = data.url
-        }
 
         document.querySelector('.date').innerHTML = data.date;
 
@@ -23,11 +37,35 @@ fetch ('https://api.nasa.gov/planetary/apod?date=2022-03-30&api_key=Blevdngru4uX
         if(data.hasOwnProperty('copyright') === true){
         document.querySelector('.copyright').innerHTML = data.copyright;}
 
+        //if APOD is a video
+
+
+        }else if (data.media_type === 'video'){
+            document.querySelector('iframe').classList.toggle('invisible');
+            document.querySelector('iframe').src = data.url;
+            document.querySelector('img').src = '';
+
+            document.querySelector('.date').innerHTML = data.date;
+
+            document.querySelector('p').innerHTML = data.explanation;
+    
+            if(data.hasOwnProperty('copyright') === true){
+            document.querySelector('.copyright').innerHTML = data.copyright;}
+
+            //if APOD is outside of date range
+
+        }else{
+            document.querySelector('h2').innerHTML = data.msg
+        }
+
+        
+
     })
     .catch (err => {
         console.log(`error ${err}`)
     })
 
+}
 
     //if (data.media_type = 'video')
     //if(data.media_type = 'image')
