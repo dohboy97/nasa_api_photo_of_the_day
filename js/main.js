1e29993073d19f9adbc7268d5c7d38fef1be7a7d
Connor <connor.doherty97@gmail.com>
2022-04-14 16:15:25 -0400

    completed project. upgraded the ui to look somewhat decent, mostly just css touch ups

--- js/main.js
+++ js/main.js
@@ -1,4 +1,76 @@
 
+
+//TODAYS PHOTO OF THE DAY
+
+document.querySelector('.today').addEventListener('click', todaysPhoto)
+
+function todaysPhoto(){
+    fetch ('https://api.nasa.gov/planetary/apod?api_key=Blevdngru4uXheqWFtLTAe7qMdXMox5NJj8nf0of')
+    .then (res => res.json())
+    .then (data =>{
+        console.log(data);
+
+        //clearing everything on initial run
+        document.querySelector('img').src = '';
+
+        document.querySelector('.date').innerHTML = '';
+
+        document.querySelector('p').innerHTML = '';
+
+        document.querySelector('.copyright').innerHTML = ''
+
+        document.querySelector('h2').innerHTML = data.title;
+
+        
+
+        //if APOD is an img
+
+
+        if(data.media_type === 'image'){
+        document.querySelector('img').src = data.hdurl;
+
+        document.querySelector('.date').innerHTML = data.date;
+        document.querySelector('iframe').classList.add('invisible')
+        document.querySelector('p').innerHTML = data.explanation;
+
+        if(data.hasOwnProperty('copyright') === true){
+        document.querySelector('.copyright').innerHTML = data.copyright;}
+
+        //if APOD is a video
+
+
+        }else if (data.media_type === 'video'){
+            document.querySelector('iframe').classList.toggle('invisible');
+            document.querySelector('iframe').src = data.url;
+            document.querySelector('img').src = '';
+
+            document.querySelector('.date').innerHTML = data.date;
+
+            document.querySelector('p').innerHTML = data.explanation;
+    
+            if(data.hasOwnProperty('copyright') === true){
+            document.querySelector('.copyright').innerHTML = data.copyright;}
+
+            //if APOD is outside of date range
+
+        }else{
+            document.querySelector('h2').innerHTML = data.msg
+        }
+
+        
+
+    })
+    .catch (err => {
+        console.log(`error ${err}`)
+    })
+    
+}
+
+
+
+
+//CUSTOM PICKED PHOTO OF THE DAY
+
 document.querySelector('.submit').addEventListener('click', getAPOD)
 
 function getAPOD(){
@@ -28,6 +100,9 @@ fetch (`https://api.nasa.gov/planetary/apod?date=${date}&api_key=Blevdngru4uXheq
 
 
         if(data.media_type === 'image'){
+
+           
+            
         document.querySelector('img').src = data.hdurl;
 
         document.querySelector('.date').innerHTML = data.date;
@@ -66,6 +141,3 @@ fetch (`https://api.nasa.gov/planetary/apod?date=${date}&api_key=Blevdngru4uXheq
     })
 
 }
-
-    //if (data.media_type = 'video')
-    //if(data.media_type = 'image')
